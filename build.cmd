e83759ee3c686a48b942ff999168bcb0a2ada568
pifopi <dottel.gael@gmail.com>
2021-05-14 15:51:00 +0200

    update exe name

--- build.cmd
+++ build.cmd
@@ -3,9 +3,9 @@ pushd %~dp0
 mkdir bin
 cd bin
 cmake ../src/ -G "Visual Studio 16" -DIMGUI_DIR=%~dp0/src/imgui/
-MSBuild.exe .\main.vcxproj /p:Configuration=Debug
-MSBuild.exe .\main.vcxproj /p:Configuration=Release
-MSBuild.exe .\main.vcxproj /p:Configuration=RelWithDebInfo
-MSBuild.exe .\main.vcxproj /p:Configuration=MinSizeRel
+MSBuild.exe .\zelda.vcxproj /p:Configuration=Debug
+MSBuild.exe .\zelda.vcxproj /p:Configuration=Release
+MSBuild.exe .\zelda.vcxproj /p:Configuration=RelWithDebInfo
+MSBuild.exe .\zelda.vcxproj /p:Configuration=MinSizeRel
 
 popd
